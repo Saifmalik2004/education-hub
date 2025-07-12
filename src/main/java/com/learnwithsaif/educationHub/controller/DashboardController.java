b5bc939166ba758cec9d05c226e64368d1170e5c
saifmalik2004 <saifmalik7827@gmail.com>
2025-07-12 15:44:13 +0530

    ready for deployment

--- src/main/java/com/learnwithsaif/educationHub/controller/DashboardController.java
+++ src/main/java/com/learnwithsaif/educationHub/controller/DashboardController.java
@@ -24,12 +24,7 @@ public class DashboardController {
 
      @Autowired
     Environment environment; 
-    
-    @Value("${eduhub.pageSize}")
-    private int defaultPageSize;
-
-    @Value("${eduhub.contact.successMsg}")
-    private String message;
+   
 
     @RequestMapping("/dashboard")
     public String displayDashboard(Model model,Authentication authentication,HttpSession session) {
@@ -40,22 +35,9 @@ public class DashboardController {
             model.addAttribute("enrolledClass", person.getHubClass().getName());
         }
         session.setAttribute("loggedInPerson", person);
-        logMessages();
         return "dashboard.html";
     }
 
-     private void logMessages() {
-          log.warn("Warning message from the Dashboard page");
-        log.info("Info message from the Dashboard page");
-        log.debug("Debug message from the Dashboard page");
-        log.trace("Trace message from the Dashboard page");
-
-        log.error("defaultPageSize value with @Value annotation is : "+defaultPageSize);
-        log.error("successMsg value with @Value annotation is : "+message);
-
-        log.error("defaultPageSize value with Environment is : "+environment.getProperty("eduhub.pageSize"));
-        log.error("successMsg value with Environment is : "+environment.getProperty("eduhub.contact.successMsg"));
-        log.error("Java Home environment variable using Environment is : "+environment.getProperty("JAVA_HOME"));
-    }
+    
 
 }
